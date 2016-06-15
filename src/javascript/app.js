f7313822ff64232810467df20557879047fd5dbc
John Martin <curmudgeon+git@rallydev.com>
2016-06-15 10:46:33 -0700

    try having heights automatic

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -336,8 +336,8 @@ Ext.define("TSDependencyByPI", {
             padding: 5,
             margin: 10,
             width: 300,
-            height: 400,
-            overflowY: 'auto'
+            //height: 400,
+            //overflowY: 'auto'
         });
         
         var iteration_start = iteration.StartDate || "--";
